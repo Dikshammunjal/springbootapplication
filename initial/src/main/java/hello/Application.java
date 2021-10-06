23a10b511e13d13b174a078251d08d7f90dc6c3d
Dikshammunjal <57708209+Dikshammunjal@users.noreply.github.com>
2021-10-06 16:42:34 +0530

    Update Application.java

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -13,7 +13,14 @@ public class Application {
 	public String home() {
 		return "<h1>Spring Boot Hello World!</h1>";
 	}
-
+        @RequestMapping("/generateLogs")
+        public String generateLogs() {
+                System.out.println("Generating some test logs for verification");
+                for( int i=0;i<10;i++){
+                   System.out.println("Test Log statement:"+i);
+                }
+                return "<h1>Generated some sample log statement </h1>";
+        }
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
